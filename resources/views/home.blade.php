86e4666d547109a4b38053c53d32ee332b974086
Glenn Hermans <glhermans@gmail.com>
2016-01-17 09:46:46 +0100

    Added authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-10 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Hulpmiddelen</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    You are logged in!
                 </div>
             </div>
         </div>
