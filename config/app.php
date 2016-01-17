3ea4e0f4677e09191f7f0e566a889c447146edad
Glenn Hermans <glhermans@gmail.com>
2016-01-17 14:57:38 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -93,7 +93,7 @@ return [
 
     'key' => env('APP_KEY'),
 
-    'cipher' => 'AES-256-CBC',
+    'cipher' => 'PVoC0fZcruOoHPEOXXawcHbEXkF7LJwT',
 
     /*
     |--------------------------------------------------------------------------
