16972d90b0a7f34eb78644b20cbbeec3f380eebd
Glenn Hermans <glhermans@gmail.com>
2016-01-17 14:32:02 +0100

    Removed comments

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -5,5 +5,5 @@ mkdir ~/node-latest-install
 cd ~/node-latest-install
 curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
 ./configure --prefix=~/local
-make install # ok, fine, this step probably takes more than 30 seconds...
+make install
 curl https://www.npmjs.org/install.sh | sh
